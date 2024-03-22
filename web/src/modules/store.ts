ad2b785ef944c1d4d71c2d5a1b4652a13ea070f1
shirokuro-bit <Desktop>
2024-03-22 20:01:11 +0900

    feat: 既存のeffectTextにあたるReduxのSliceを作成

--- web/src/modules/store.ts
+++ web/src/modules/store.ts
@@ -3,12 +3,14 @@ import {configureStore} from "@reduxjs/toolkit";
 import imageItemsSlice from "./imageItemsSlice.ts";
 import selectIdSlice from "./selectIdSlice.ts";
 import editModeSlice from "./editModeSlice.ts";
+import effectTextSlice from "./effectTextSlice.ts";
 
 export const store = configureStore({
   reducer: {
     imageItems: imageItemsSlice,
     selectId: selectIdSlice,
-    editMode: editModeSlice
+    editMode: editModeSlice,
+    effectText: effectTextSlice
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware({
     serializableCheck: false
